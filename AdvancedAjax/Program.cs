346df475a68b77e53de3bc6ea75bbeaec1566491
Opilane TTHK <svjatoslavrazumovski@gmail.com>
2025-05-16 10:04:36 +0300

    Ajax proj. 3

--- AdvancedAjax/Program.cs
+++ AdvancedAjax/Program.cs
@@ -13,6 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetSection("ConnectionStrings:DbConn").Value));
 
 var app = builder.Build();
 
